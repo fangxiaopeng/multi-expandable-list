0fad327a6ddf970ed102fd066f20e0da2d92485e
fangxiaopeng <850899969@qq.com>
2018-03-02 13:52:41 +0800

    新建ExpandableListViewAdapter.java

---
+++ android/MultiExpandableList/app/src/main/java/fxp/com/multiexpandablelist/adapter/ExpandableListViewAdapter.java
@@ -0,0 +1,65 @@
+package fxp.com.multiexpandablelist.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+/**
+ *
+ * ExpandableListView 适配器
+ *
+ * Created by fxp on 2018/3/2.
+ */
+
+public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+}
